d2f1a237b2b634fa61c2b13ebc1303bff6754f7e
MrNaif2018 <chuff184@gmail.com>
2021-01-20 15:22:47 +0300

    Fixes for SDK 1.0

--- bitccl/ext/sdk.py
+++ bitccl/ext/sdk.py
@@ -1,6 +1,6 @@
 import warnings
 
-from bitcart import coins
+from bitcart import COINS
 
 from ..utils import mark_allowed_imports
 
@@ -9,8 +9,7 @@ def startup():
     context = {}
     with warnings.catch_warnings():  # for coins init without xpub
         warnings.simplefilter("ignore")
-        for coin_name in coins.__all__:
-            coin_class = getattr(coins, coin_name)
+        for coin_name, coin_class in COINS.items():
             context[coin_name] = mark_allowed_imports(coin_class)
             context[coin_name.lower()] = mark_allowed_imports(coin_class())
     return context
--- tests/ext/test_sdk.py
+++ tests/ext/test_sdk.py
@@ -1,18 +1,12 @@
 """Our SDK is tested too, so we test only basic integration"""
 
 import pytest
-from bitcart import coins
+from bitcart import COINS
 
 from bitccl import run
 
 
-@pytest.mark.parametrize("obj", coins.__all__)
+@pytest.mark.parametrize("obj", COINS)
 def test_scope_exists(obj):
     assert run(obj) is None
     assert run(obj.lower()) is None
-
-
-@pytest.mark.parametrize("obj", coins.__all__)
-def test_anonymous_coins(obj):
-    with pytest.warns(UserWarning):
-        assert run(f"assert {obj.lower()}.xpub == {obj}().xpub") is None  # ensure lowercase coin names have no xpub set
